71dc9300010836e2d458d287635256a85f8c4894
Sean Park <chanseanpark@gmail.com>
2020-12-29 02:24:04 -0800

    created authRoutes file for spotify

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get(
+    '/auth/spotify',
+    passport.authenticate('spotify', {
+      scope: ['user-read-email', 'user-read-private'],
+      showDialog: true,
+    })
+  );
+
+  app.get(
+    '/auth/spotify/callback',
+    passport.authenticate('spotify', { failureRedirect: '/auth/error' }),
+    function (req, res) {
+      res.redirect('/');
+    }
+  );
+
+  app.get('/auth/error', (req, res) => res.send('Unknown Error'));
+};
