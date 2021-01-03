2e3cc9a7032d3e204e34403e5f318b54383db1b6
Sean Park <chanseanpark@gmail.com>
2021-01-02 23:58:07 -0800

    add LogIn component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import Header from './Header';
+import LogIn from './LogIn';
 
 function App() {
   return (
     <Container>
       <Header />
+      <LogIn />
     </Container>
   );
 }
